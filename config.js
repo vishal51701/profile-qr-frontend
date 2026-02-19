083afafc5b7a2f93054f250d80d9e05258162bdb
vishal51701 <22p61a0517@vbithyd.ac.in>
2026-02-19 22:58:24 +0530

    fix api endpoint

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-const API_BASE_URL =
-  "https://digital-profile-backend-production.up.railway.app/api/profile";
-
+const API_BASE_URL = "https://digital-profile-backend-production.up.railway.app";
 export default API_BASE_URL;
